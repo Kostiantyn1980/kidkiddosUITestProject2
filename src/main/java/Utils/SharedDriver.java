7931948483dc3bf4fee2521518f3eec6938c74b9
Kostiantyn1980 <123832686+Kostiantyn1980@users.noreply.github.com>
2023-11-18 12:00:01 -0700

    Update SharedDriver.java
    upd

--- src/main/java/Utils/SharedDriver.java
+++ src/main/java/Utils/SharedDriver.java
@@ -2,6 +2,7 @@ package Utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import java.util.concurrent.TimeUnit;
